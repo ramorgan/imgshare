eb1d5c036b4f6f9fa5058985b18fe647e27176df
Reese Morgan <ramorgan212@gmail.com>
2017-11-01 20:26:46 -0400

    inital commit

---
+++ src/imgshare/App.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace imgshare;
+
+use Silex\Application as SilexApplication;
+
+class App extends SilexApplication
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
