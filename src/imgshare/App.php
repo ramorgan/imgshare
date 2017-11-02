db5598797816dabe3d4002e9c6b819b7d6a17b67
Reese Morgan <ramorgan212@gmail.com>
2017-11-01 21:35:33 -0400

    use the imgshare\App as the application.

--- src/imgshare/App.php
+++ src/imgshare/App.php
@@ -9,6 +9,20 @@ class App extends SilexApplication
     public function __construct()
     {
         parent::__construct();
+
+
+        $this->registerRoutes($this);
+    }
+
+
+    protected function registerRoutes(App $app)
+    {
+        $app->get(
+            '/',
+            function () use ($app) {
+                return "Welcome to your new Silex Application!";
+            }
+        )->bind('homepage');
     }
 
 }
--- web/index.php
+++ web/index.php
@@ -4,9 +4,10 @@ ini_set('display_errors', 0);
 if (!file_exists(__DIR__ . '/../config/settings.json')) {
     die("config/settings.json does not exist");
 }
+
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = require __DIR__.'/../src/app.php';
-require __DIR__.'/../config/prod.php';
-require __DIR__.'/../src/controllers.php';
+ini_set('display_errors', 'On');
+$app = new imgshare\App();
+$app['debug'] = true;
 $app->run();
