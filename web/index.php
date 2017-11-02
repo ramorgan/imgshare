5b263d86470e2694cf3232314ad3a1df4e02f73b
Reese Morgan <ramorgan212@gmail.com>
2017-11-01 21:24:04 -0400

    add check for config file

--- web/index.php
+++ web/index.php
@@ -1,7 +1,9 @@
 <?php
 
 ini_set('display_errors', 0);
-
+if (!file_exists(__DIR__ . '/../config/settings.json')) {
+    die("config/settings.json does not exist");
+}
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../src/app.php';
